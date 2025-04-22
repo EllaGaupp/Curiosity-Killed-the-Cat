7fa4628e41b47861e7b033e0a802a1d362ee85f6
andre <ellagaupp@gmail.com>
2025-04-22 13:32:52 -0500

    Added initial text and ability to begin game

--- src/project.py
+++ src/project.py
@@ -1,4 +1,7 @@
 import pygame
+import time
+
+user_input = ""
 
 def main():
     pygame.init()
@@ -8,11 +11,17 @@ def main():
     screen = pygame.display.set_mode(resolution)
     running = True
     color = (0,0,0)
+    font = pygame.font.Font(None, 24)
+    text = font.render("Curiosity Killed the Cat", True, (255,0,0))
+    text2 = font.render("Press ENTER to Start", True, (255,0,0))
+    gameStart = False
+    
     while running:
         # Event Loops
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+       
         key = pygame.key.get_pressed()
         # Make fullscreen
         if key[pygame.K_f]:
@@ -25,7 +34,27 @@ def main():
             screen = pygame.display.set_mode(resolution)
             pygame.display.update()
         screen.fill(color)
+        # Adds text to display
+        screen_width, screen_height = screen.get_size()
+        text_rect = text.get_rect(center=(screen_width // 2, screen_height // 2))
+        screen.blit(text, text_rect)
+        text_rect = text.get_rect(center=(screen_width // 2 + 10, screen_height // 2 + 50))
+        screen.blit(text2, text_rect)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                gameStart = True
+            
+        if gameStart:
+            # Makes sure text is centered 
+            screen.fill(color)
+            screen_width, screen_height = screen.get_size()
+            text_rect = text.get_rect(center=(screen_width // 2, screen_height // 2 + 150)) 
+            # Instructions to player
+            text = font.render("Game started", True, (255,0,0))
+            screen.blit(text, text_rect)
+
         pygame.display.flip()
 
+        
 if __name__ == "__main__":
     main()
